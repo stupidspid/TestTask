78b7e8444dada39e7aac9a500d44fd8a930a827f
NUTS\natas <natasha_minskaya@mail.ru>
2024-02-15 17:58:58 +0300

    feat: add necessary conditions

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services;
+
+public class OrderService : IOrderService
+{
+    private readonly ApplicationDbContext _db;
+
+    public OrderService(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    
+    public async Task<Order> GetOrder()
+    {
+        var orderWithHighestCost = await _db.Orders
+            .OrderByDescending(o => o.Quantity * o.Price)
+            .FirstOrDefaultAsync();
+
+        return orderWithHighestCost;
+    }
+
+    public async Task<List<Order>> GetOrders()
+    {
+        return await _db.Orders
+            .Where(o => o.Quantity > 10)
+            .ToListAsync();
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services;
+
+public class UserService : IUserService
+{
+    private readonly ApplicationDbContext _db;
+
+    public UserService(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    public async Task<User> GetUser()
+    {
+        var userWithMostOrders = await _db.Orders
+            .GroupBy(order => order.UserId) 
+            .Select(group => new
+            {
+                UserId = group.Key,
+                TotalOrders = group.Count()
+            })
+            .OrderByDescending(group => group.TotalOrders)
+            .FirstOrDefaultAsync();
+
+        var userValue = await _db.Users.FirstOrDefaultAsync(u=> userWithMostOrders.UserId == u.Id);
+        return userValue;
+    }
+
+    public async Task<List<User>> GetUsers()
+    {
+        return await _db.Users
+            .Where(u => u.Status == UserStatus.Inactive)
+            .ToListAsync();
+    }
+}
